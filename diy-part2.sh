c5ee20d54a361341916fe4103cd90bb5cb1d30dc
siddhix <41867661+siddhix@users.noreply.github.com>
2023-12-04 14:44:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,6 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/10.10.10.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
